f726b14227aca76e52ea0f4c3f0db6d76ca03957
JKJUN527 <631642753@qq.com>
2018-05-02 14:31:34 +0800

    Update master.blade.php
    加入百度站长工具自动提交网址

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -109,6 +109,21 @@
             $(".taoyige").css("border-right","0px");
         });
 </script>
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+
 @section('custom-script')
 @show
 </body>
